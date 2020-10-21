0d027a9b8f3153bb4b08b4274862b2fa061e7f9f
bug <hugebug4ever@126.com>
2020-10-21 21:50:49 +0800

    fix launch script

--- config/生成code_cs_bin和data_bin.bat
+++ config/生成code_cs_bin和data_bin.bat
@@ -1,3 +1,13 @@
-..\src\Luban.Client\bin\Debug\netcoreapp3.1\gen.client -h 127.0.0.1 -j cfg -- -d Defines/root.xml --input_data_dir Datas --output_code_dir output_code --output_data_dir output_data -s server --gen_types code_cs_bin,data_bin --export_test_data
+..\src\Luban.Client\bin\Debug\netcoreapp3.1\Luban.Client.exe ^
+	-h 127.0.0.1 ^
+	-j cfg ^
+	-- ^
+	-d Defines/root.xml ^
+	--input_data_dir Datas ^
+	--output_code_dir output_code ^
+	--output_data_dir output_data ^
+	-s server ^
+	--gen_types code_cs_bin,data_bin ^
+	--export_test_data
 
 pause
