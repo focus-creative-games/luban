3e6105124729f78006aa11a393d788cd78ddbd4a
walon <taojingjian@gmail.com>
2020-11-28 20:00:06 +0800

    [change] remove EMBED_CFG macro

--- src/Luban.Server/Source/Program.cs
+++ src/Luban.Server/Source/Program.cs
@@ -44,11 +44,11 @@ namespace Luban.Server
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
             GenServer.Ins.Start(options.Port, ProtocolStub.Factories);
-#if EMBED_CFG
+
             GenServer.Ins.RegisterJob("cfg", new Luban.Job.Cfg.JobController());
-#endif
             GenServer.Ins.RegisterJob("proto", new Luban.Job.Proto.JobController());
             GenServer.Ins.RegisterJob("db", new Luban.Job.Db.JobController());
+
             int processorCount = System.Environment.ProcessorCount;
             ThreadPool.SetMinThreads(Math.Max(4, processorCount), 5);
             ThreadPool.SetMaxThreads(Math.Max(16, processorCount * 4), 10);
