10c2768f1aa297beeb349dd27c7a454bc4aabe90
walon <taojingjian@gmail.com>
2021-06-19 16:22:10 +0800

    【新增】新增Excel2TextDiff工具项目，用于将excel转换为text或者直接将两个excel文件转换为text后再diff

---
+++ src/Excel2TextDiff/Excel2TextWriter.cs
@@ -0,0 +1,61 @@
+﻿using CommandLine;
+using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel2TextDiff
+{
+    class Excel2TextWriter
+    {
+        class CommandLineOptions
+        {
+            [Option('p', "port", Required = false, HelpText = "listen port")]
+            public int Port { get; set; } = 8899;
+        }
+
+        public void TransformToTextAndSave(string excelFile, string outputTextFile)
+        {
+            var lines = new List<string>();
+            using var excelFileStream = new FileStream(excelFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            string ext = Path.GetExtension(excelFile);
+            using (var reader = ext != ".csv" ? ExcelReaderFactory.CreateReader(excelFileStream) : ExcelReaderFactory.CreateCsvReader(excelFileStream))
+            {
+                do
+                {
+                    lines.Add($"===[{reader.Name ?? ""}]===");
+                    LoadRemainRows(reader, lines);
+                } while (reader.NextResult());
+            }
+            File.WriteAllLines(outputTextFile, lines, System.Text.Encoding.UTF8);
+        }
+
+        private void LoadRemainRows(IExcelDataReader reader, List<string> lines)
+        {
+            int rowIndex = 0;
+            while (reader.Read())
+            {
+                ++rowIndex; // 第一行是 meta ，跳过
+                var row = new List<string>();
+                for (int i = 0, n = reader.FieldCount; i < n; i++)
+                {
+                    object cell = reader.GetValue(i);
+                    row.Add(cell != null ? cell.ToString() : "");
+                }
+                int lastNotEmptyIndex = row.FindLastIndex(s => !string.IsNullOrEmpty(s));
+                if (lastNotEmptyIndex >= 0)
+                {
+                    row = row.GetRange(0, lastNotEmptyIndex + 1);
+                }
+                else
+                {
+                    row.Clear();
+                }
+                lines.Add(string.Join(',', row));
+            }
+        }
+    }
+}
---
+++ src/Excel2TextDiff/Program.cs
@@ -0,0 +1,90 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Excel2TextDiff
+{
+    class CommandLineOptions
+    {
+        [Option('t', SetName = "transform", HelpText = "transform excel to text file")]
+        public bool IsTransform { get; set; }
+
+        [Option('d', SetName = "diff", HelpText = "transform and diff file")]
+        public bool IsDiff { get; set; }
+
+        [Option('p', SetName = "diff", Required = false, HelpText = "3rd diff program. default TortoiseMerge")]
+        public string DiffProgram { get; set; }
+
+        [Value(0)]
+        public IList<string> Files { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (args.Length != 2 && args.Length != 3)
+            //{
+            //    Console.WriteLine("Usage:");
+            //    Console.WriteLine("Excel2TextDiff <file1> <file2> [path_of_diff_program]");
+            //    return;
+            //}
+
+            var options = ParseOptions(args);
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            var writer = new Excel2TextWriter();
+
+            if (options.IsTransform)
+            {
+                if (options.Files.Count != 2)
+                {
+                    Console.WriteLine("Usage: Excel2TextDiff -t <excel file> <text file>");
+                    Environment.Exit(1);
+                }
+
+                writer.TransformToTextAndSave(options.Files[0], options.Files[1]);
+            }
+            else
+            {
+                if (options.Files.Count != 2)
+                {
+                    Console.WriteLine("Usage: Excel2TextDiff -d <excel file 1> <excel file 2> [-p <diff programe>]");
+                    Environment.Exit(1);
+                }
+
+                var diffProgame = options.DiffProgram ?? "TortoiseMerge.exe";
+
+                var tempTxt1 = Path.GetTempFileName();
+                writer.TransformToTextAndSave(options.Files[0], tempTxt1);
+
+                var tempTxt2 = Path.GetTempFileName();
+                writer.TransformToTextAndSave(options.Files[1], tempTxt2);
+
+                string arg1 = $"/base:{tempTxt1.Replace('\\', '/')}";
+                string arg2 = $"/mine:{tempTxt2.Replace('\\', '/')}";
+                Console.WriteLine(" {0} {1}", arg1, arg2);
+                System.Diagnostics.Process.Start(diffProgame, new string[] { arg1, arg2 });
+            }
+        }
+
+        private static CommandLineOptions ParseOptions(String[] args)
+        {
+            var helpWriter = new StringWriter();
+            var parser = new Parser(ps =>
+            {
+                ps.HelpWriter = helpWriter;
+            });
+
+            var result = parser.ParseArguments<CommandLineOptions>(args);
+            if (result.Tag == ParserResultType.NotParsed)
+            {
+                Console.Error.WriteLine(helpWriter.ToString());
+                Environment.Exit(1);
+            }
+            return ((Parsed<CommandLineOptions>)result).Value;
+        }
+    }
+}
