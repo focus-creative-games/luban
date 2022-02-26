0e3782dd48609b140072311eda9a10d659568888
walon <taojingjian@gmail.com>
2022-02-26 11:04:16 +0800

    【调整】config gen_gype 'code_cs_json' 改名为 'code_cs_dotnet_json'，与'code_cs_unity_json' 区分，避免误用

--- src/Luban.Job.Cfg/Source/Generate/CsCodeDotNetJsonRender.cs
+++ src/Luban.Job.Cfg/Source/Generate/CsCodeDotNetJsonRender.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 
 namespace Luban.Job.Cfg.Generate
 {
-    [Render("code_cs_json")]
-    class CsCodeJsonRender : TemplateCodeRenderBase
+    [Render("code_cs_dotnet_json")]
+    class CsCodeDotNetJsonRender : TemplateCodeRenderBase
     {
-        protected override string RenderTemplateDir => "cs_json";
+        protected override string RenderTemplateDir => "cs_dotnet_json";
     }
 }
