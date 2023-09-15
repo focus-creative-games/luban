6aa553e3e1ff15841f18bf09e8a6fd4c68536456
宝鱼 <kteong1012@outlook.com>
2023-09-15 14:48:47 +0800

    [new] 新增数据类型cs-lazyload-bin和代码目标cs-lazyload-bin。
    Signed-off-by: 宝鱼 <kteong1012@outlook.com>

--- src/Luban.DataTarget.Builtin/Binary/BinaryRecordOffsetLengthDataTarget.cs
+++ src/Luban.DataTarget.Builtin/Binary/BinaryRecordOffsetLengthDataTarget.cs
@@ -1,19 +1,22 @@
-using Luban.Datas;
+﻿using Luban.Datas;
 using Luban.DataTarget;
 using Luban.Defs;
 using Luban.Serialization;
+using YamlDotNet.Core.Tokens;
 
 namespace Luban.DataExporter.Builtin.Binary;
 
-[DataTarget("bin-offset")]
-public class BinaryRecordOffsetDataTarget : DataTargetBase
+[DataTarget("bin-offsetlength")]
+public class BinaryRecordOffsetLengthDataTarget : DataTargetBase
 {
     protected override string OutputFileExt => "bytes";
-    
+
     private void WriteList(DefTable table, List<Record> datas, ByteBuf x)
     {
-        ByteBuf buf = new ByteBuf();
+        ByteBuf buf = new ByteBuf(10 * 1024);
+        x.WriteSize(datas.Count);
         buf.WriteSize(datas.Count);
+        int lastOffset = 0;
         foreach (var d in datas)
         {
             foreach (var indexInfo in table.IndexList)
@@ -21,8 +24,12 @@ public class BinaryRecordOffsetDataTarget : DataTargetBase
                 DType keyData = d.Data.Fields[indexInfo.IndexFieldIdIndex];
                 keyData.Apply(BinaryDataVisitor.Ins, x);
             }
-            x.WriteSize(buf.Size);
+            int offset = buf.Size;
+            x.WriteSize(offset);
             d.Data.Apply(BinaryDataVisitor.Ins, buf);
+            int length = buf.Size - lastOffset;
+            x.WriteSize(length);
+            lastOffset = buf.Size;
         }
     }
 
