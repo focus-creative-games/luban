1593e3a6a30f0d64d733ad47f48b777a5aec2576
kdletters <945792343@qq.com>
2024-04-30 16:38:22 +0800

    rust导出初步完成，缺少多态支持

---
+++ src/Luban.Rust/TemplateExtensions/RustJsonTemplateExtension.cs
@@ -0,0 +1,32 @@
+﻿using Luban.Rust.TypeVisitors;
+using Luban.Types;
+using Scriban.Runtime;
+
+namespace Luban.Rust.TemplateExtensions;
+
+public class RustJsonTemplateExtension : ScriptObject
+{
+    public static string Deserialize(string fieldName, string jsonVarName, TType type)
+    {
+        if (type.IsNullable)
+        {
+            return $"let mut {fieldName} = serde_json::from_value({jsonVarName}.clone())";
+        }
+        else
+        {
+            return $"let {fieldName} = {type.Apply(RustJsonUnderlyingDeserializeVisitor.Ins, $"{jsonVarName}[\"{fieldName}\"]", fieldName, 0)};";
+        }
+    }
+
+    public static string DeserializeField(string fieldName, string jsonVarName, TType type)
+    {
+        if (type.IsNullable)
+        {
+            return $"let mut {fieldName} = None; if let Some(value) = {jsonVarName}.get(\"{fieldName}\") {{ {fieldName} = Some({type.Apply(RustJsonUnderlyingDeserializeVisitor.Ins, $"{jsonVarName}[\"{fieldName}\"]", fieldName, 0)}); }}";
+        }
+        else
+        {
+            return $"let {fieldName} = {type.Apply(RustJsonUnderlyingDeserializeVisitor.Ins, $"{jsonVarName}[\"{fieldName}\"]", fieldName, 0)};";
+        }
+    }
+}
