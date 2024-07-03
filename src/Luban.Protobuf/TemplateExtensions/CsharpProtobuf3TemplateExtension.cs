f61ab6dbb96d248a9b3ab31713756536bade1ebb
unknown <x826190447@126.com>
2024-07-03 11:35:21 +0800

    add cs-protobuf

---
+++ src/Luban.Protobuf/TemplateExtensions/CsharpProtobuf3TemplateExtension.cs
@@ -0,0 +1,32 @@
+﻿using Scriban.Runtime;
+namespace Luban.Protobuf.TemplateExtensions;
+
+public  class CsharpProtobuf3TemplateExtension : ScriptObject
+{
+
+    public static string NamespaceWithGraceBegin(string ns)
+    {
+        if (string.IsNullOrEmpty(ns))
+        {
+            return string.Empty;
+        }
+        ns = ns.Substring(0, 1).ToUpper() + ns.Substring(1);
+
+        return $"namespace {ns}\n{{";
+    }
+
+    public static string NamespaceWithGraceEnd(string ns)
+    {
+        if (string.IsNullOrEmpty(ns))
+        {
+            return string.Empty;
+        }
+        return "}";
+    }
+    public static string ProtoFullName(string typeName)
+    {
+        var name = string.Join("", typeName.Split('.'));
+
+        return name.Substring(0, 1).ToUpper() + name.Substring(1);
+    }
+}
