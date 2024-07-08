161f1d191184f61739905336845a027b60e40484
unknown <x826190447@126.com>
2024-07-08 18:21:32 +0800

    add cs-newton-json code target

---
+++ src/Luban.CSharp/CodeTarget/CsharpNewtonJsonCodeTarget.cs
@@ -0,0 +1,15 @@
+﻿using Luban.CodeTarget;
+using Luban.CSharp.TemplateExtensions;
+using Scriban;
+
+namespace Luban.CSharp.CodeTarget;
+
+[CodeTarget("cs-newton-json")]
+public class CsharpNewtonJsonCodeTarget : CsharpCodeTargetBase
+{
+    protected override void OnCreateTemplateContext(TemplateContext ctx)
+    {
+        base.OnCreateTemplateContext(ctx);
+        ctx.PushGlobal(new CsharpNewtonJsonTemplateExtension());
+    }
+}
