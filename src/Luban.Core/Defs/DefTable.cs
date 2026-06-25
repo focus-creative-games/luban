663ace32f476130bd22c08cd489c7d723e3f36b5
absences <49706424+absences@users.noreply.github.com>
2026-06-25 15:53:38 +0800

    fix: throw not exported exception when index field isnot exported and host bean is exprted(#254)

--- src/Luban.Core/Defs/DefTable.cs
+++ src/Luban.Core/Defs/DefTable.cs
@@ -112,16 +112,12 @@ public class DefTable : DefTypeBase
                 {
                     if (ValueTType.DefBean.TryGetField(Index, out var f, out var i))
                     {
-                        if (f.NeedExport())
-                        {
-                            IndexField = f;
-                            IndexFieldIdIndex = i;
-                        }
-                        else
+                        if(!f.NeedExport() && this.NeedExport())
                         {
                             throw new Exception($"table:'{FullName}' 索引{f.Name}不能导出，请指定有效索引");
                         }
-
+                        IndexField = f;
+                        IndexFieldIdIndex = i;
                     }
                     else
                     {
@@ -136,7 +132,7 @@ public class DefTable : DefTypeBase
                 {
                     var f = ValueTType.DefBean.HierarchyFields[0];
 
-                    if (!f.NeedExport())
+                    if (!f.NeedExport() && this.NeedExport())
                     {
                         throw new Exception($"table:'{FullName}' 默认索引{f.Name}不能导出，请指定有效索引");
                     }
